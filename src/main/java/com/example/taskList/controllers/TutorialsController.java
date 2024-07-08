8fca678c73a114b54519abd3c620490a69c2e5ef
FranciscoJChCano <franciscojcharnecocano@gmail.com>
2024-07-08 11:17:15 +0200

    Edit controller and models

--- src/main/java/com/example/taskList/controllers/TutorialsController.java
+++ src/main/java/com/example/taskList/controllers/TutorialsController.java
@@ -1,5 +1,59 @@
 package com.example.taskList.controllers;
 
-public class TutorialsController {
+
     
+
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.taskList.models.Tutorials;
+import com.example.taskList.service.TutorialService;
+
+@RestController
+@RequestMapping("/tutorial")
+public class TutorialsController {
+
+    @Autowired
+    private TutorialService TutorialService;
+
+    @GetMapping
+    public ArrayList<Tutorials> getUsers() {
+        return this.userService.getUsers();
+    }
+
+    @PostMapping
+    public Tutorials saveUser(@RequestBody Tutorials tutorial) {
+        return this.userService.saveUser(tutorial);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Tutorials> getUserById(@PathVariable("id") Long id) {
+        return this.userService.getById(id);
+    }
+
+    @PutMapping
+    public Tutorials updateTutorial(@RequestBody Tutorials tutorial) {
+        return this.userService.updateById(tutorial);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteById(@PathVariable("id") Long id) {
+        boolean ok = this.userService.deleteUser(id);
+
+        if (ok)
+            return "User with id " + id + " as deleted";
+        else
+            return "Error, we have a problem to deleted user " + id;
+    }
 }
---
+++ src/main/java/com/example/taskList/models/Tutorials.java
@@ -0,0 +1,59 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tutorials")
+public class Tutorials {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private Boolean published;
+
+
+    public Boolean getPublished() {
+        return published;
+    }
+
+    public void setPublished(Boolean published) {
+        this.published = published;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+ 
+}
